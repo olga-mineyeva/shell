e9adcfa73c3b6319d556a7526ab3f88c3c5bbeeb
Olga Mineyeva <o.mineyeva@gmail.com>
2024-08-21 20:09:24 -0400

    updated file on desktop

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -18,7 +18,7 @@ touch file1 file2 file3 file4 file5 / dir2
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
 
 # 6. Delete file4
-
+rm dir2/file4
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
